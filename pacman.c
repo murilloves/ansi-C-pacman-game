00b1ff9b9005010b844c2b39ac1a1e8ccae0c67b
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2017-02-07 17:16:23 -0200

    Create pacman.c

---
+++ pacman.c
@@ -0,0 +1,357 @@
+/*       \n        ||
+ASCII:
+      Seta Direita:   77   =   'l'
+      Seta Esquerda:  75   =   'j'
+      Seta Baixo:     80   =   'k'
+      Seta Cima:      72   =   'i'
+      
+      WE GOTTA POWER: -11 -72 -101
+      
+PACMAN RUDIMENTAR
+Version 0.3
+
+*/
+
+#include <stdio.h>
+
+
+#define DIM 15
+#define COL 11
+#define WALL -80
+
+
+    typedef struct fase{
+            int fase[DIM][COL];
+            }Tfase;
+    Tfase lvl_1;
+
+    char *phant, ch, PACMAN[DIM][DIM], aux, aux2=' ';
+    char  Maux=' ';
+    int i, c, col=DIM/2, lin=DIM-1, pts=0, remaing=0, M1=DIM/2, M2=DIM/2, move=0, life = 3;
+
+/*
+
+Inicia variáveis
+
+*/
+void var_zera()
+{
+     PACMAN[DIM][DIM], aux, aux2=' ';
+     Maux=' ';
+     col=DIM/2; 
+     lin=DIM-1; 
+     pts=0; 
+     remaing=0;
+     M1=DIM/2; 
+     M2=DIM/2; 
+     move=0; 
+     life = 3;     
+}
+/*
+
+Zerando a Matriz(PACMAN)
+
+*/
+void zeraMatriz()
+{
+    for(i=0; i<DIM; i++){
+             for(c=0; c<DIM; c++){
+             PACMAN[i][c]=' ';
+             }
+    }
+}
+
+/*
+
+Colocando os pontos
+
+*/
+void criaField(int x, int y){
+    for(x=0; x<DIM; x++){
+             for(y=0; y<COL; y++){
+             if(y==1&&x!=0&&x!=DIM-1&&x!=DIM/2||y==COL-2&&x!=0&&x!=DIM-1&&x!=DIM/2||x==1&&y!=COL-1&&y!=0&&y!=COL/2||x==DIM-2&&y!=0&&y!=COL/2&&y!=COL-1){ 
+                                         lvl_1.fase[x][y]=2;
+                                         }
+             else if(x==0&&y==0||x==0&&y==COL-1||x==DIM-1&&y==COL-1||x==DIM-1&&y==0){
+                  lvl_1.fase[x][y]=2;
+                  }
+             else {
+                   lvl_1.fase[x][y]=1;
+                   remaing++;
+                  }
+             }
+    }
+
+          // Posiçao PacChuqMan e seus amiguenhos Ghosts
+             PACMAN[DIM-1][DIM/2]='<';
+             PACMAN[DIM/2][DIM/2]='M';
+             phant= &PACMAN[DIM-1][DIM/2];
+}
+/*
+
+TELA DE BOAS VINDAS
+
+*/
+void telaOp(){
+     printf("    __   __   __          __             \n");
+     printf("   |  | |  | |    |\\  /| |  | |\\  |     \n");
+     printf("   |__| |__| |    | \\/ | |__| | \\ |     \n");
+     printf("   |    |  | |    |    | |  | |  \\|      \n");
+     printf("   |    |  | |__  |    | |  | |   |      v. 0.3\n\n");
+     printf("   Aperte Enter para comecar a jogar.\n\n1 Player <start>\n2 Player <start>\n\n\nby: Murillo Vieira");
+getchar();
+}
+
+/*
+
+IMPRIMINDO A CADA VEZ
+
+*/
+void insprime(){
+
+   system("cls");
+
+   printf(" *Vidas: ");
+   for(i=0; i<life; i++){
+       printf("%c", 3);
+       }
+   printf("\n\n *Pontos: %4d\n\n",pts);
+
+          for(i=0; i<DIM; i++){
+             for(c=0; c<COL; c++){
+                 if(lvl_1.fase[i][c]==2)
+                    printf("%c",WALL);
+                 else
+                    printf(" ");
+             }
+          printf("\n");
+          }
+
+}
+
+/*
+
+PHANTON'S  PART 
+
+*/
+void phanton_move(){
+move=0;
+if(ch==75||ch==77||ch==80||ch==72){
+   if(M1==lin){
+      if(M2<col&&PACMAN[M1][M2+1]!=-79){
+      PACMAN[M1][M2]=Maux;
+      M2++; move++;
+      Maux= PACMAN[M1][M2];
+            if(PACMAN[M1][M2]!=' '&&PACMAN[M1][M2]!='.')
+               Maux=' ';
+      phant= &PACMAN[M1][M2];
+      PACMAN[M1][M2]='M';
+      }
+
+      if(M2>col&&PACMAN[M1][M2-1]!=-79){
+      PACMAN[M1][M2]=Maux;
+      M2--;move++;
+      Maux= PACMAN[M1][M2];
+            if(PACMAN[M1][M2]!=' '&&PACMAN[M1][M2]!='.')
+               Maux=' ';
+      phant= &PACMAN[M1][M2];
+      PACMAN[M1][M2]='M';
+      }
+   }
+   
+   else if(M1<lin&&PACMAN[M1+1][M2]!=-79&&move==0){
+   PACMAN[M1][M2]=Maux;
+   M1++;move++;
+   Maux= PACMAN[M1][M2];
+            if(PACMAN[M1][M2]!=' '&&PACMAN[M1][M2]!='.')
+               Maux=' ';
+   phant= &PACMAN[M1][M2];
+   PACMAN[M1][M2]='M';
+   }
+   
+   else if(M1>lin&&PACMAN[M1-1][M2]!=-79&&move==0){
+   PACMAN[M1][M2]=Maux;
+   M1--;move++;
+   Maux= PACMAN[M1][M2];
+            if(PACMAN[M1][M2]!=' '&&PACMAN[M1][M2]!='.')
+               Maux=' ';
+   phant= &PACMAN[M1][M2];
+   PACMAN[M1][M2]='M';
+   }
+
+   if(move==0&&phant!= &PACMAN[lin][col]){
+
+      if(M2<col&&PACMAN[M1][M2+1]!=-79){
+      PACMAN[M1][M2]=Maux;
+      M2++;move++;
+      Maux= PACMAN[M1][M2];
+            if(PACMAN[M1][M2]!=' '&&PACMAN[M1][M2]!='.')
+               Maux=' ';
+      phant= &PACMAN[M1][M2];
+      PACMAN[M1][M2]='M';
+      }
+      
+      if(M2>col&&PACMAN[M1][M2-1]!=-79){
+      PACMAN[M1][M2]=Maux;
+      M2--;move++;
+      Maux= PACMAN[M1][M2];
+            if(PACMAN[M1][M2]!=' '&&PACMAN[M1][M2]!='.')
+               Maux=' ';
+      phant= &PACMAN[M1][M2];
+      PACMAN[M1][M2]='M';
+      }
+  }
+  if(move==0)
+      if(M2==col||M1==lin){
+           if(PACMAN[M1-1][M2]!=-79){
+              PACMAN[M1][M2]=Maux;
+              M1--;move++;
+              Maux= PACMAN[M1][M2];
+            if(PACMAN[M1][M2]!=' '&&PACMAN[M1][M2]!='.')
+               Maux=' ';
+              phant= &PACMAN[M1][M2];
+              PACMAN[M1][M2]='M';
+           }
+          if(PACMAN[M1+1][M2]!=-79&&move==0){
+              PACMAN[M1][M2]=Maux;
+              M1++;move++;
+              Maux= PACMAN[M1][M2];
+            if(PACMAN[M1][M2]!=' '&&PACMAN[M1][M2]!='.')
+               Maux=' ';
+              phant= &PACMAN[M1][M2];
+              phant= &PACMAN[M1][M2];
+              PACMAN[M1][M2]='M';
+           }
+       }
+   }  
+}
+
+/* 
+
+Morreu 
+
+*/
+void dead_check(){
+     
+    if(M1==lin&&M2==col){
+           life--;
+           if(life==0){
+           system ("cls");
+           printf("\n GAME OVER! \n\n - Aperte qualquer tecla para jogar outra vez.");
+           getchar();
+           main();
+           }
+           
+           printf("\n- YOU ARE DEAD -\n\n\n Faca como o Raul Seixas e 'Tente outra vez!'");
+           PACMAN[lin][col]=' ';    PACMAN[M1][M2]=' ';    PACMAN[DIM-1][DIM/2]='<';    lin=DIM-1;    col=DIM/2;     move=-1;
+            getch();
+     }
+}
+
+/* 
+
+Movimentação PACMAN 
+
+*/
+void pacman_move(){
+    if(ch==77&&col!=DIM-1&&PACMAN[lin][col+1]!=-79){
+               if(PACMAN[lin][col+1]=='.'){
+                  remaing--;
+                  pts= pts+10;
+                  }
+               if(PACMAN[lin][col+1]==-72){
+                  pts= pts+100;
+                  }
+               if(col < DIM-1)col++;
+               if(col!=DIM){
+                      PACMAN[lin][col]='<';
+                      PACMAN[lin][col-1]=' ';
+                      }
+               }
+    if(ch==75&&col!=0&&PACMAN[lin][col-1]!=-79){
+               if(PACMAN[lin][col-1]=='.'){
+                  remaing--;
+                  pts= pts+10;
+                  }
+               if(PACMAN[lin][col-1]==-72){
+                  pts= pts+100;
+                  }
+               if(col > 0)col--;
+                      PACMAN[lin][col]='>';
+                      PACMAN[lin][col+1]=' ';
+               }
+    if(ch==80&&lin!=DIM-1&&PACMAN[lin+1][col]!=-79){
+               if(PACMAN[lin+1][col]=='.'){
+                  remaing--;
+                  pts= pts+10;
+                  }
+               if(PACMAN[lin+1][col]==-72){
+                  pts= pts+100;
+                  }
+               if(lin < DIM-1)lin++;
+                      PACMAN[lin][col]='^';
+                      PACMAN[lin-1][col]=' ';
+               }
+    if(ch==72&&lin!=0&&PACMAN[lin-1][col]!=-79){
+               if(PACMAN[lin-1][col]=='.'){
+                  remaing--;
+                  pts= pts+10;
+                  }
+               if(PACMAN[lin-1][col]==-72){
+                  pts= pts+100;
+                  }
+               if(lin > 0)lin--;
+                      PACMAN[lin][col]='v';
+                      PACMAN[lin+1][col]=' ';
+               }
+}
+
+/*
+
+TELA VITÓRIA
+
+*/
+void check_win(){
+     if(remaing==2){
+
+   system("cls");
+   printf("----------  Voce Ganhou UM MILHAO DE REAIS EM BARRAS DE OURO \n----------  Que valem mais do que dinheiroo! ----------");
+   printf("\n\n\n -- YOU WIN -- ");
+   getchar();
+   exit(1);
+   }
+}
+
+void EXIT_GAME(){
+     system("cls");
+
+     printf("Deseja realmente sair?\n - (Y) = Sim\n - (N) = Nao");
+     ch=getch();
+     if(ch=='y')exit(0);
+     else main();
+}
+   
+int main(){
+    
+    system("cls");
+
+    var_zera();
+    zeraMatriz();
+    criaField(i,c);
+    telaOp();
+
+    do{
+      ch = getch();
+
+      phanton_move();
+      pacman_move();
+      insprime();
+      check_win();
+      dead_check();
+
+    }while(ch!='q');
+
+    var_zera();
+    EXIT_GAME();
+
+}
